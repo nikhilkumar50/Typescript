25984fa5e04557de05bf4962ecd3f8dfbfef54a0
nikhilkumar50 <nikhilkumarray9889@gmail.com>
2024-06-21 00:28:47 +0530

    array,data types

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+let a = "hello";
+console.log(a);
+const names = ["nikhil", "kumar", "raj"];
+console.log(names);
+names.push("45");
+const numbers = [12, 15, 18];
+console.log(numbers);
+const values = [5, 10, 15, 20, 25];
+const namesstring = ["nikhil", "kumar"];
+const namestring2 = ["a", "c"];
+console.log(values);
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+let a="hello";
+console.log(a);
+
+const names:string[]=["nikhil","kumar","raj"];
+console.log(names);
+
+names.push("45");
+
+const numbers:number[]=[12,15,18];
+console.log(numbers);
+
+
+const values: ReadonlyArray<number>=[5,10,15,20,25];
+
+const namesstring:readonly string[]=["nikhil","kumar"];
+const namestring2:ReadonlyArray<string>=["a","c"];
+
+console.log(values);
+
